8119a7c967559e8199997ffe47ab1a979263bc10
hikarinessa <nessa_vardalmir@hotmail.com>
2021-12-06 20:10:33 +0100

    Update advent_03.py

--- advent_2021_hikarinessa/advent_03.py
+++ advent_2021_hikarinessa/advent_03.py
@@ -47,26 +47,58 @@ def find_rates_one(input):
 
 # part 2
 def find_rates_two(input):
-    oxygen_generator_rating = input.duplicate()
-    co2_scrubber_rating = input.duplicate()
-    halflength = len(input)/2
+    oxygen_generator_rating = input.copy()
+    co2_scrubber_rating = input.copy()
 
-    for i in range(0, len(input[0])):
+    for i in range(0, len(oxygen_generator_rating[0])):
         bit = 0
 
-        for j in input:
-            bit += int(j[i])
+        for j in oxygen_generator_rating:
+                bit += int(j[i])
 
-        for k in input:
-            if bit >= halflength:
-                oxygen_generator_rating[k].pop()
-            else:
-                co2_scrubber_rating
+        if bit >= len(oxygen_generator_rating)/2: # 1 is more prevalent
+            for ind, k in enumerate(oxygen_generator_rating):
+                if k is not None and int(k[i]) == 0:
+                    oxygen_generator_rating[ind] = None
+        else:  # 0 is more prevalent
+            for ind, k in enumerate(oxygen_generator_rating):
+                if k is not None and int(k[i]) == 1:
+                    oxygen_generator_rating[ind] = None
 
-    life_support_rating = ''
+        oxygen_generator_cleanup = [k for k in oxygen_generator_rating if k]
+        oxygen_generator_rating = oxygen_generator_cleanup
+        if len(oxygen_generator_cleanup) == 1:
+            break
+    oxygen_generator_rating = int(oxygen_generator_rating[0], 2)
+    # print("oxygen_generator_rating", oxygen_generator_rating)
+
+    for i in range(0, len(co2_scrubber_rating[0])):
+        bit = 0
+
+        for j in co2_scrubber_rating:
+                bit += int(j[i])
+
+        if bit >= len(co2_scrubber_rating)/2: # 1 is more prevalent
+            for ind, k in enumerate(co2_scrubber_rating):
+                if k is not None and int(k[i]) == 1:
+                    co2_scrubber_rating[ind] = None
+        else:  # 0 is more prevalent
+            for ind, k in enumerate(co2_scrubber_rating):
+                if k is not None and int(k[i]) == 0:
+                    co2_scrubber_rating[ind] = None
+
+        co2_scrubber_cleanup = [k for k in co2_scrubber_rating if k]
+        co2_scrubber_rating = co2_scrubber_cleanup
+        if len(co2_scrubber_rating) == 1:
+            break
+    co2_scrubber_rating = int(co2_scrubber_rating[0], 2)
+    # print("co2_scrubber_rating", co2_scrubber_rating)
+
+
+    life_support_rating = oxygen_generator_rating * co2_scrubber_rating
     return life_support_rating
 
 
 print('Part 1 =', find_rates_one(INPUT))  # 4174964
 print('*'*30)
-print('Part 2 =', '')  #
+print('Part 2 =', find_rates_two(INPUT))  # 4474944
